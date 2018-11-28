b7f5d48be7f8c120dd9e980b0641eaf572fde9f1
Wilson Ortiz <wilson.ortiz@equifax.com>
2018-11-28 09:18:54 -0300

    first commit

---
+++ src/main/java/com/springboot/backend/apirest/app/SpringBootBackendApirestApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.backend.apirest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendApirestApplication.class, args);
+	}
+}
